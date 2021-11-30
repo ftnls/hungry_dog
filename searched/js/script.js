9a50132403b6096715d2962a8ea6aeae47ed2c0f
tymophiyy <tima.matviiv@gmail.com>
2021-11-30 21:58:52 +0200

    did css for each product and started doing js

--- searched/js/script.js
+++ searched/js/script.js
@@ -2,3 +2,7 @@ $( ".plus-product" ).on( "click", function () {
 	let value = $(".search-input").val();
 	alert(value);
 });
+
+$(".products #1", ).on("click", function () {
+	$(".product").remove();
+});
