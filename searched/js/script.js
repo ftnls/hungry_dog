f22294dc24d35f7f2b798c9eccf0a0f2c26d25f2
tymophiyy <tima.matviiv@gmail.com>
2021-12-01 15:00:35 +0200

    Try create black products

--- searched/js/script.js
+++ searched/js/script.js
@@ -1,8 +1,25 @@
+
+let id = 0;
 $( ".plus-product" ).on( "click", function () {
-	let value = $(".search-input").val();
-	alert(value);
+	search_value = $(".search-input").val();
+	var element_product = "<div onclick=\"whatId(this.id)\" class=\"product\" id=" + id + 
+	"><p onclick=\"whatId(this.id)\" id=" + id + 
+	">" + search_value + "</p></div>";
+	
+	$(".products").append(element_product);
+	id += 1;
+
+	$(".search-input").val("");
 });
 
-$(".products #1", ).on("click", function () {
-	$(".product").remove();
-});
+function whatId(clicked_id) {
+	// alert(clicked_id);
+	$(".products #" + clicked_id).remove();
+}
+
+
+
+// $(".product").on("click", function () {
+// 	alert(id);
+// 	$(".product #" + id).remove();
+// });
