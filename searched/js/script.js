66110e95a6b4bedd383494596001c773438f9959
Kaziavka <lygadanyloapp@gmail.com>
2021-12-02 23:48:11 +0200

    added function for creating elemnts from search input, and deleting on click

--- searched/js/script.js
+++ searched/js/script.js
@@ -1,25 +1,13 @@
+const productlist = document.getElementById("products-list");
 
-let id = 0;
-$( ".plus-product" ).on( "click", function () {
-	search_value = $(".search-input").val();
-	var element_product = "<div onclick=\"whatId(this.id)\" class=\"product\" id=" + id + 
-	"><p onclick=\"whatId(this.id)\" id=" + id + 
-	">" + search_value + "</p></div>";
-	
-	$(".products").append(element_product);
-	id += 1;
-
-	$(".search-input").val("");
-});
-
-function whatId(clicked_id) {
-	// alert(clicked_id);
-	$(".products #" + clicked_id).remove();
+function findeHandler() {
+  const value = document.getElementById("search-input").value;
+  const li = document.createElement("li");
+  li.innerText = value;
+  li.className = "product";
+  li.id = value;
+  li.onclick = function () {
+    this.parentElement.removeChild(this);
+  };
+  productlist.appendChild(li);
 }
-
-
-
-// $(".product").on("click", function () {
-// 	alert(id);
-// 	$(".product #" + id).remove();
-// });
