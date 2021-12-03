a05ce5702ff72b665a7cb297c7e68cbd9089f2f1
tymophiyy <tima.matviiv@gmail.com>
2021-12-03 03:00:17 +0200

    Add backlight for recipe blocks and add event when you direct cursor on the recipe

--- searched/js/script.js
+++ searched/js/script.js
@@ -3,13 +3,18 @@ const productlist = document.getElementById("products-list");
 function findeHandler() {
   const value = document.getElementById("search-input").value;
   const li = document.createElement("li");
-  li.innerText = value;
-  li.className = "product";
-  li.id = value;
-  li.onclick = function () {
-    this.parentElement.removeChild(this);
-  };
-  productlist.appendChild(li);
+  if (value.length > 0) {
+    li.innerText = value;
+    li.className = "product";
+    li.id = value;
+    li.onclick = function () {
+      this.parentElement.removeChild(this);
+    };
+    productlist.appendChild(li);
+    document.getElementById("search-input").value = "";
+  } else {
+    console.log("You have to enter something in search input!!!");
+  }
 }
 
 // Get the input field
@@ -18,6 +23,9 @@ var input = document.getElementById("search-input");
 input.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
     findeHandler();
-    document.getElementById("search-input").value = "";
   }
 });
+
+function showProducts() {
+
+}
